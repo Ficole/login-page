acba69f850763acbbcdc00708f2a81d5531d2748
Ficole <46519221+Ficole@users.noreply.github.com>
2019-01-11 09:28:29 +0100

    Add files via upload

---
+++ login/login.php
@@ -0,0 +1,32 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Document</title>
+    <link rel="stylesheet" href="login.css">
+    <link rel="stylesheet" href="styleee.css">
+    <link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.6.3/css/all.css" integrity="sha384-UHRtZLI+pbxtHCWp1t77Bi1L4ZtiqrqD80Kn4Z8NTSRyMA2Fd33n5dQ8lWUE00s/" crossorigin="anonymous">
+
+</head>
+<body>
+    <div id="one">
+        <h2>Mission To Deep Space</h2>
+        <div id="three"><i class="fas fa-cog fa-10x"></i></div>
+        <P>The Future is Here</P>
+    </div>
+    <div id="two"> 
+        <div><h3>Sign Up</h3>
+            <form>
+                <br><input class="i1" type="text" placeholder="Username"><br><br>
+                <br><input class="i2" type="password" placeholder="Password"><br><br>
+                <br><input class="i3" type="email" placeholder="Email"><br><br>
+                <input class="i4" type="submit"value="Join">
+            
+                
+           </form>
+        </div>
+    </div>
+</body>
+</html>
